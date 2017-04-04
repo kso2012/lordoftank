1325e126560a0dd62a7109c019191e7b49971742
SYJ <modori439@gmail.com>
2017-04-04 09:41:10 +0900

    멀티 게임모드 추가

---
+++ LordOfTank/Source/LordOfTank/Public/GameMode/MultiGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MultiGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LORDOFTANK_API AMultiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
