44e8c3b7a811976212c997b56f3e42c17663f111
feck2918@naver.com <feck2918@naver.com>
2017-06-09 05:58:21 +0900

    아이템 박스와 낙하산 생성, 아이템 박스 물리 적용
    아이템 박스가 생성될 때 낙하산이 커지고 지면과 일정 거리 내에 있으면 낙하산을 접고 떨어지는 속도가 빨라짐. 랜드에 충돌

---
+++ LordOfTank/Source/LordOfTank/Public/Item/Item.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Item.generated.h"
+
+UCLASS()
+class LORDOFTANK_API AItem : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class UStaticMeshComponent* BoxMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* ParachuteMesh;
+
+	float FallingSpeed;
+
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void SetFallingSpeed();
+
+	UBoxComponent* CollBox;
+
+	UFUNCTION()
+		void CheckFalling(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	bool bIsFalling;
+
+	bool bUseGravity;
+
+	void CheckLocation();
+
+	float Gravity;
+
+	void ShakeBox();
+
+	bool bShakePlus;
+
+	float ParachuteSize;
+
+	void ScaleUpParachute();
+
+	void ScaleDownParachute();
+	
+};
